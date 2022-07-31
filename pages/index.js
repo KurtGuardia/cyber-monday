f9930bb97cd62543633786800ad78d306fa27847
Kurt Guardia <kaguardiagamarra@wideagency.com>
2022-07-31 18:08:17 +0200

    almost done

---
+++ pages/index.js
@@ -0,0 +1,30 @@
+import Search from 'components/Search.jsx'
+import Head from 'next/head'
+// import Image from 'next/image'
+
+import Footer from '../components/Footer.jsx'
+import MainLogo from '../components/MainLogo.jsx'
+import HomeResults from '../components/HomeResults.jsx'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>¡Black Friday Autocomplete App!</title>
+        <meta
+          name='description'
+          content='Black Friday Autocomplete App'
+        />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+
+      <main>
+        <MainLogo />
+        <Search />
+        <HomeResults />
+      </main>
+
+      <Footer />
+    </div>
+  )
+}
